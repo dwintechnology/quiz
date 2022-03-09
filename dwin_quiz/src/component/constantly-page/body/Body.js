2134233d72983cdd2fcbeb96170a3a8c659c4cfa
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-03-09 11:41:14 +0400

    create page information geography quiz

--- dwin_quiz/src/component/constantly-page/body/Body.js
+++ dwin_quiz/src/component/constantly-page/body/Body.js
@@ -1,4 +1,5 @@
 import { Route, Routes,Navigate } from 'react-router-dom';
+import GeographyQuizPage from '../../geography-quiz-page/GeographyQuizPage';
 import HomePage from '../../homePage/HomePage'
 import SelectTopic from '../../select-topic-page/SelectTopicPage';
 import './Body.scss'
@@ -11,6 +12,7 @@ export default function Body() {
                 <Route path='/' element={<Navigate to ='home'/>}/>
                 <Route path='/home' element={<HomePage/>} />
                 <Route path='/selectTopic' element={<SelectTopic/>} />
+                <Route path='/selectTopic/geography' element={<GeographyQuizPage/>} />
                 {/* <Route path='/*' element={} */}
             </Routes>
         </div>
