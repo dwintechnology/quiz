016046c46dd3af5aabcacd3e31b348ee2d6ba46d
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-03-13 23:38:22 +0400

    created a all quiz of 5 questions, after the answers it shows how many correct answers from 5 questions

--- dwin_quiz/src/component/constantly-page/body/Body.js
+++ dwin_quiz/src/component/constantly-page/body/Body.js
@@ -1,18 +1,10 @@
 import React from "react"
 import { Route, Routes, Navigate } from 'react-router-dom';
-
-import AgricultureQuizInformationPage from '../../topic-information-pages/agriculture-quiz-information-page/AgricultureQuizInformationPage';
-import FootballQuizInformationPage from '../../topic-information-pages/football-quiz-information-page/FootballQuizInformationPage';
-import HistoryQuizInformationPage from '../../topic-information-pages/history-quiz-information-page/HistoryQuizInformationPage';
-import GeographyQuizInformationPage from '../../topic-information-pages/geography-quiz-information-page/GeographyQuizInformationPage';
-import TechnologyQuizInformationPage from '../../topic-information-pages/technology-quiz-information-page/TechnologyQuizInformationPage';
-import MedicalQuizInformationPage from '../../topic-information-pages/medical-quiz-information-page/MedicalQuizInformationPage';
-
+import TopicInformationPage from '../../topic-information-pages/TopicInformationPage';
 import HomePage from '../../home-page/HomePage'
-import SelectTopic from '../../select-topic-page/SelectTopicPage';
+import Topics from '../../select-topic-page/SelectTopicPage';
 import './Body.scss'
-import Timer from "../../timer/Timer";
-import MedicalQuiz from "../../quiz-pages/medical-quiz-page/MedicalQuizPage";
+import Quiz from '../../quiz-pages/Quiz'
 
 export default function Body() {
     return (
@@ -21,15 +13,9 @@ export default function Body() {
             <Routes>
                 <Route path='/' element={<Navigate to='home' />} />
                 <Route path='/home' element={<HomePage />} />
-                <Route path='/selectTopic' element={<SelectTopic />} />
-                <Route path='/selectTopic/agriculture' element={<AgricultureQuizInformationPage />} />
-                <Route path='/selectTopic/history' element={<HistoryQuizInformationPage />} />
-                <Route path='/selectTopic/technology' element={<TechnologyQuizInformationPage />} />
-                <Route path='/selectTopic/Football' element={<FootballQuizInformationPage />} />
-                <Route path='/selectTopic/medical' element={<MedicalQuizInformationPage />} />
-                <Route path='/selectTopic/geography' element={<GeographyQuizInformationPage />} />
-                <Route path='/support' element={<Timer />} />
-                <Route path='/medicalQuiz' element={<MedicalQuiz />} />
+                <Route path='/topics' element={<Topics />} />
+                <Route path='/topic/:name' element={<TopicInformationPage/>} />   
+                <Route path='/quiz/:name' element={<Quiz />} />
                 {/* <Route path='/*' element={}/> */}
             </Routes>
         </div>
--- dwin_quiz/src/component/quiz-pages/Quiz.js
+++ dwin_quiz/src/component/quiz-pages/Quiz.js
@@ -1,41 +1,101 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { useParams } from 'react-router-dom';
 import './Quiz.scss'
 import Timer from "../timer/Timer"
 import Medal1 from '../../img/medal-1.svg'
 import Medal2 from '../../img/medal-2.svg'
 import { useNavigate } from 'react-router';
-import PropTypes from 'prop-types';
+import MedicalImg from '../../img/Medical.jpg';
+import GeographyImg from '../../img/Geography.jpg';
+import AgricultureImg from '../../img/Agriculture.jpg';
+import HistoryImg from '../../img/History.jpg';
+import TechnologyImg from '../../img/Technology.jpg';
+import FootballImg from '../../img/Football.jpg';
+import questions from '../data/Data'
 
 
-export default function Quiz({ title, questions, img }) {
 
+export default function Quiz() {
 
+    let { name } = useParams()
+    const navigate = useNavigate();
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const [showScore, setShowScore] = useState(false)
     const [isCorrectVal, setIsCorrectVal] = useState(0)
+    const [quizTitle, setQuizTitle] = useState()
+    const [quizImg, setQuizImg] = useState()
+    const [dataQuestion, setDataQuestion] = useState([])
+
+    let arrQuestions = questions.filter((el) => el.title === name)
 
 
-    const handledAnswerCheckBoxClick = (val) => {
+    useEffect(() => {
+        switch (name) {
+            case 'medical':
+                setQuizTitle('Medical')
+                setQuizImg(MedicalImg)
+                setDataQuestion(arrQuestions)
+                break
 
-        if (val === true) {
+            case 'geography':
+                setQuizTitle('Geography')
+                setQuizImg(GeographyImg)
+                setDataQuestion(arrQuestions)
+                break
+
+            case 'agriculture':
+                setQuizTitle('Agriculture')
+                setQuizImg(AgricultureImg)
+                setDataQuestion(arrQuestions)
+                break
+
+            case 'history':
+                setQuizTitle('History')
+                setQuizImg(HistoryImg)
+                setDataQuestion(arrQuestions)
+                break
+
+            case 'technology':
+                setQuizTitle('Technology')
+                setQuizImg(TechnologyImg)
+                setDataQuestion(arrQuestions)
+                break
+
+            case 'football':
+                setQuizTitle('Football')
+                setQuizImg(FootballImg)
+                setDataQuestion(arrQuestions)
+                break
+
+        }
+
+    }, [])
+
+
+
+    const handledAnswerCheckBoxClick = (value) => {
+
+        if (value === true) {
             setIsCorrectVal(isCorrectVal + 1)
         }
 
         const nextCurrent = currentQuestion + 1
-        if (nextCurrent === questions.length) {
+        console.log(nextCurrent)
+        if (nextCurrent === dataQuestion[0]?.question.length) {
             setShowScore(true)
         }
         setCurrentQuestion(nextCurrent)
     }
-    const navigate = useNavigate();
+
 
 
     return (
+
         <div className='quiz'>
             {showScore ? (
                 <>
                     <div className='score-section'>
-                        You scored {isCorrectVal} out of {questions.length}
+                        You scored {isCorrectVal} out of {dataQuestion[0]?.question.length}
 
                         {(isCorrectVal >= 3 && isCorrectVal < 5) &&
                             <div className='medalImg'>
@@ -54,7 +114,7 @@ export default function Quiz({ title, questions, img }) {
                             </div>}
                         <button
                             className='buttonGoNewQuiz'
-                            onClick={() => navigate('/selectTopic')}>
+                            onClick={() => navigate('/topics')}>
                             Go New Quiz</button>
 
                     </div>
@@ -64,7 +124,7 @@ export default function Quiz({ title, questions, img }) {
                     <div className='title'>
 
                         <div>
-                            <h1>{title}</h1>
+                            <h1>{quizTitle} Quiz</h1>
                             <h4> Read the following instructions </h4>
                         </div>
                         <div className='timer'>
@@ -73,14 +133,14 @@ export default function Quiz({ title, questions, img }) {
                     </div>
                     <div className='bodyQuiz'>
                         <div className='imgQuiz'>
-                            <img src={img} alt='imgQuiz' />
+                            <img src={quizImg} alt='imgQuiz' />
                         </div>
-                        <div className='quiz'>
+                        <div className='question'>
 
-                            <h2>Questions {currentQuestion + 1}/{questions.length}</h2>
+                            <h2>Questions {currentQuestion + 1}/{dataQuestion[0]?.question.length}</h2>
 
-                            <div>
-                                {questions[currentQuestion].questionsText}
+                            <div >
+                                {dataQuestion[0]?.question[currentQuestion]?.questionsText}
                             </div>
 
                         </div>
@@ -88,8 +148,8 @@ export default function Quiz({ title, questions, img }) {
                     </div>
 
                     <div className='answer'>
-                        {questions[currentQuestion].answerOption.map((answerOption, i) =>
-                            <button onClick={() => handledAnswerCheckBoxClick(answerOption.isCorrect)} key={i}>{answerOption.answerText}</button>
+                        {dataQuestion[0]?.question[currentQuestion]?.answerOption?.map((answerOption, i) =>
+                            <button onClick={() => handledAnswerCheckBoxClick(answerOption?.isCorrect)} key={i}> {answerOption?.answerText} </button>
 
                         )}
                     </div>
@@ -99,9 +159,3 @@ export default function Quiz({ title, questions, img }) {
     )
 }
 
-Quiz.propTypes = {
-    title: PropTypes.string,
-    questions: PropTypes.arrayOf(PropTypes.object),
-    img: PropTypes.element
-
-};
